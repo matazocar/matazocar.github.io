9031d6aefc1e5fd642416c97fa8a213cf2fc9001
matazocar <mat.azocar.c@gmail.com>
2026-06-15 22:15:59 +0200

    Add files via upload

--- build.js
+++ build.js
@@ -203,6 +203,7 @@ function escapeAttr(s) {
 // Frontmatter parsing
 // ---------------------------------------------------------------
 function parseFrontmatter(raw) {
+  raw = raw.replace(/\r\n/g, "\n");
   const match = raw.match(/^---\n([\s\S]*?)\n---\n?([\s\S]*)$/);
   if (!match) {
     return { meta: {}, body: raw };
